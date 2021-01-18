f6eba2caec0ac16e2a9bf90bac329c088554a129
Clydies Freeman <clyde@joinsoko.com>
2021-01-18 02:22:08 -0500

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by clydies freeman on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
